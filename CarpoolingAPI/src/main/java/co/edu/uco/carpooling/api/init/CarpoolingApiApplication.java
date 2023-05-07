844c7c31dbce406bd01deddf53f7dcc1b78ae958
Federico zapata <fede.zapata16@gmail.com>
2023-05-07 09:31:30 -0500

    Commit inicial, se creo proyecto CarpoolingApiApplication

---
+++ CarpoolingAPI/src/main/java/co/edu/uco/carpooling/api/init/CarpoolingApiApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.carpooling.api.init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpoolingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarpoolingApiApplication.class, args);
+    }
+
+}
